d20372e15658c810ca2f26fe6d3093fac2a20036
Artem Tkachuk <arti17900@gmail.com>
2019-09-28 20:03:59 +0600

    Added CRUD for working with comments

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -1,22 +1,8 @@
-"""blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from webapp.views import IndexView, ArticleView, ArticleCreateView, \
-    ArticleUpdateView, ArticleDeleteView, CommentCreateView
+    ArticleUpdateView, ArticleDeleteView, CommentCreateView, CommentView, CommentUpdateView, CommentDeleteView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +11,8 @@ urlpatterns = [
     path('article/add/', ArticleCreateView.as_view(), name='article_add'),
     path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
     path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
-    path('comment/add/', CommentCreateView.as_view(), name='comment_add')
+    path('comment/add/', CommentCreateView.as_view(), name='comment_add'),
+    path('comments/', CommentView.as_view(), name='comments'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -93,7 +93,45 @@ class CommentCreateView(View):
                 text=form.cleaned_data['text'],
                 article=form.cleaned_data['article']
             )
-            # это нужно исправить на ваш url.
             return redirect('article_view', pk=comment.article.pk)
         else:
             return render(request, 'comment/create.html', context={'form': form})
+
+
+class CommentView(TemplateView):
+    template_name = 'comment/comments_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.all().order_by('-created_at')
+        return context
+
+
+class CommentUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data={
+            'article': comment.article.id,
+            'author': comment.author,
+            'text': comment.text
+        })
+        return render(request, 'comment/update.html', context={'form': form, 'comment': comment})
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment.article = form.cleaned_data['article']
+            comment.author = form.cleaned_data['author']
+            comment.text = form.cleaned_data['text']
+            comment.save()
+            return redirect('comments')
+        else:
+            return render(request, 'comment/update.html', context={'form': form, 'comment': comment})
+
+
+class CommentDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        comment.delete()
+        return redirect('comments')
