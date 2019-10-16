12969ba09c5e190eb65a2a642a3c50fb313fda34
Artem Tkachuk <arti17900@gmail.com>
2019-10-16 21:49:42 +0600

    Формы переделаны на модельные.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,24 +1,18 @@
 from django import forms
-from django.forms import widgets
 
-from webapp.models import Category, Article
-
-
-class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Title')
-    author = forms.CharField(max_length=40, required=True, label='Author')
-    text = forms.CharField(max_length=3000, required=True, label='Text',
-                           widget=widgets.Textarea)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False, label='Category',
-                                      empty_label=None)
+from webapp.models import Article, Comment
 
 
-class CommentForm(forms.Form):
-    article = forms.ModelChoiceField(queryset=Article.objects.all(), required=True, label='Article',
-                                     empty_label=None)
-    author = forms.CharField(max_length=40, required=False, label='Author', initial='Аноним')
-    text = forms.CharField(max_length=400, required=True, label='Text',
-                           widget=widgets.Textarea)
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        exclude = ['created_at', 'updated_at']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['created_at', 'updated_at']
 
 
 class SearchForm(forms.Form):
